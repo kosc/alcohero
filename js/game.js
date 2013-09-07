47cd94bb993d87754aeb7da98357bd95995e3216
Kosenko Artyom <hotkosc@gmail.com>
2013-09-07 21:02:43 +0400

    Delete shape

--- js/game.js
+++ js/game.js
@@ -1,10 +1,5 @@
 function init()
 {
 	var stage = new createjs.Stage("gameStage");
-	var circle = new createjs.Shape();
-	circle.graphics.beginFill("green").drawCircle(0, 0, 50);
-	circle.x = 250;
-	circle.y = 100;
-	stage.addChild(circle);
 	stage.update();
 }
