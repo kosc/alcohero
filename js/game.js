55e90d9e440eb7e9cc76db71f8c7992a0d1fcf56
Kosenko Artyom <hotkosc@gmail.com>
2013-09-28 00:43:37 +0400

    Control hero with arrows

--- js/game.js
+++ js/game.js
@@ -1,30 +1,44 @@
+var SCREEN_WIDTH = 420;
+var SCREEN_HEIGHT = 600;
+var LEFT_KEY = 37;
+var RIGHT_KEY = 39;
+
+function Hero(){
+	this.img = new createjs.Bitmap("img/hero.png");
+	this.x = SCREEN_WIDTH/2 - 30;
+	this.y = 200;
+}
+
+var hero = new Hero();
+
 function game()
 {
-	var SCREEN_WIDTH = 420;
-	var SCREEN_HEIGHT = 600;
 	var stage = new createjs.Stage("gameStage");
 	var house = new createjs.Bitmap("img/house.png");
-	function hero(){
-		var scores;
-		var x, y;
-	}
+	hero.img.set({x: hero.x, y: hero.y});
 	stage.addChild(house);
 	var houseNext = house.clone();
 	houseNext.set({y: SCREEN_HEIGHT});
 	stage.addChild(houseNext);
-	stage.update();
 	var house_y = 0;
 	var houseNext_y = SCREEN_HEIGHT;
+	stage.addChild(hero.img);
+	stage.update();
 	window.setInterval(function (){
 		house_y--;
 		houseNext_y--;
 		house.set({y: house_y});
 		houseNext.set({y: houseNext_y});
+		hero.img.set({x: hero.x, y: hero.y});
 		stage.update();
 		if(house_y == -SCREEN_HEIGHT)
 			house_y = SCREEN_HEIGHT;
 		if(houseNext_y == -SCREEN_HEIGHT)
 			houseNext_y = SCREEN_HEIGHT;
 	}, 1);
-	
+}
+
+document.onkeypress = function (e){
+	if(e.keyCode == LEFT_KEY) hero.x-=5;
+	if(e.keyCode == RIGHT_KEY) hero.x+=5;
 }
