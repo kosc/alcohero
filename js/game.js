5d09f18a426e2f5ebb6f47bf2d9c7c7c00b9fbf2
Kosenko Artyom <hotkosc@gmail.com>
2013-09-28 18:17:25 +0400

    Change hero control

--- js/game.js
+++ js/game.js
@@ -2,11 +2,14 @@ var SCREEN_WIDTH = 420;
 var SCREEN_HEIGHT = 600;
 var LEFT_KEY = 37;
 var RIGHT_KEY = 39;
+var LEFT_DIRECTION = 0;
+var RIGHT_DIRECTION = 1;
 
 function Hero(){
 	this.img = new createjs.Bitmap("img/hero.png");
 	this.x = SCREEN_WIDTH/2 - 30;
 	this.y = 200;
+	this.direction = null;
 }
 
 var hero = new Hero();
@@ -25,8 +28,8 @@ function game()
 	stage.addChild(hero.img);
 	stage.update();
 	window.setInterval(function (){
-		house_y--;
-		houseNext_y--;
+		house_y-=2;
+		houseNext_y-=2;
 		house.set({y: house_y});
 		houseNext.set({y: houseNext_y});
 		hero.img.set({x: hero.x, y: hero.y});
@@ -35,10 +38,21 @@ function game()
 			house_y = SCREEN_HEIGHT;
 		if(houseNext_y == -SCREEN_HEIGHT)
 			houseNext_y = SCREEN_HEIGHT;
-	}, 1);
+		if(hero.direction == LEFT_DIRECTION){
+			hero.x-=4;
+		}
+		if(hero.direction == RIGHT_DIRECTION){
+			hero.x+=4;
+		}
+	}, 5);
 }
 
-document.onkeypress = function (e){
-	if(e.keyCode == LEFT_KEY) hero.x-=5;
-	if(e.keyCode == RIGHT_KEY) hero.x+=5;
+document.onkeydown = function (e){
+	if(e.keyCode == LEFT_KEY) hero.direction = LEFT_DIRECTION;
+	if(e.keyCode == RIGHT_KEY) hero.direction = RIGHT_DIRECTION;
+}
+
+document.onkeyup = function (e){
+	if(e.keyCode == LEFT_KEY || e.keyCode == RIGHT_KEY)
+		hero.direction = null;
 }
